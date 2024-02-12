3cc2a61431c3e398379191e6664008acf262058d
Thomas <tparisi@udel.edu>
2024-02-12 15:08:26 -0500

    added "Hello World" to site's body

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 Thomas Parisi: UD CISC275 with React Hooks and TypeScript
             </header>
+            <body>Hello World</body>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
