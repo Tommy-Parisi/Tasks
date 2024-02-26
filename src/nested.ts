9490c01ff6ab5d09eb95df4c9a9d90c8537eac49
Thomas <tparisi@udel.edu>
2024-02-25 20:07:03 -0500

    completed Q7

--- src/nested.ts
+++ src/nested.ts
@@ -77,7 +77,11 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const sumPublishedPoints = questions.reduce(
+        (total, question) => total + (question.published ? question.points : 0),
+        0
+    );
+    return sumPublishedPoints;
 }
 
 /***
