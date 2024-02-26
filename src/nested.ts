10cce67f9a23be55cad457ad75c5f3ddd6a01f9e
Thomas <tparisi@udel.edu>
2024-02-25 20:39:55 -0500

    completed Q11

--- src/nested.ts
+++ src/nested.ts
@@ -144,7 +144,14 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    if (questions.length == 0) {
+        return true;
+    }
+    const firstQuestionType = questions[0].type;
+    const allSameType = questions.every(
+        (question) => question.type === firstQuestionType
+    );
+    return allSameType;
 }
 
 /***
