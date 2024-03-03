5f36b286adcbb01e022a1fde8469263b928ac39e
Thomas <tparisi@udel.edu>
2024-03-02 19:54:40 -0500

    Completed CycleHoliday, TwoDice failing tests

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,30 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(d6);
+    const [rightDie, setRightDie] = useState<number>(() => {
+        let roll = d6();
+        while (roll === leftDie) {
+            roll = d6();
+        }
+        return roll;
+    });
+    const rollLeft = () => setLeftDie(d6());
+    const rollRight = () => setRightDie(d6());
+
+    const checkStatus = () => {
+        if (leftDie === rightDie) {
+            return leftDie === 1 ? "You Lose!" : "You Win!";
+        }
+        return "";
+    };
+    return (
+        <div>
+            <Button onClick={rollLeft}>Roll Left</Button>
+            <span data-testid="left-die">{leftDie}</span>{" "}
+            <Button onClick={rollRight}>Roll Right</Button>
+            <span data-testid="right-die">{rightDie}</span>
+            <p>{checkStatus()}</p>
+        </div>
+    );
 }
