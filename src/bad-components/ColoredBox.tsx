a63ed3a5d51d30899500931e8bc03b995ee4880b
Thomas <tparisi@udel.edu>
2024-03-04 15:35:55 -0500

    Completed ColoredBox

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -8,30 +8,34 @@ export function ColoredBox(): JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
 
     const changeColor = () => {
-        setColorIndex((colorIndex + 1) % COLORS.length);
+        const newIndex = (colorIndex + 1) % COLORS.length;
+        setColorIndex(newIndex);
+        console.log("Changing color to index:", newIndex); // Diagnostic log
     };
 
-    const ColorPreview = (
-        <div
-            data-testid="colored-box"
-            style={{
-                width: "50px",
-                height: "50px",
-                backgroundColor: COLORS[colorIndex],
-                display: "inline-block",
-                verticalAlign: "bottom",
-                marginLeft: "5px"
-            }}
-        ></div>
-    );
+    function ColorPreview() {
+        return (
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: COLORS[colorIndex],
+                    display: "inline-block",
+                    verticalAlign: "bottom",
+                    marginLeft: "5px"
+                }}
+            ></div>
+        );
+    }
 
     return (
         <div>
             <h3>Colored Box</h3>
             <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <Button onClick={() => changeColor}>Next Color</Button>
-                {ColorPreview}
+                <Button onClick={() => changeColor()}>Next Color</Button>
+                <ColorPreview />
             </div>
         </div>
     );
